4779015f71ebae5082e7c551ea279eda024470e8
Özgür Peynirci <ozgurpeynirci@gmail.com>
2025-06-15 02:38:34 +0300

    Refactor: Change UserProfile ID type from Long to String in entity and repository

--- src/main/java/com/riskengine/risksystem/repository/UserProfileRepository.java
+++ src/main/java/com/riskengine/risksystem/repository/UserProfileRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+public interface UserProfileRepository extends JpaRepository<UserProfile, String> {
     UserProfile findByUsername(String username);
     UserProfile findByEmail(String email);
 }
